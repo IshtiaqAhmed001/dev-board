2f2a54bc5cc7d09abb772be9d392c8a00e13ea77
Ishtiaq <ishtiaq.wasi@gmail.com>
2025-03-02 22:40:32 +1100

    added completed history dynamically

--- script.js
+++ script.js
@@ -6,18 +6,29 @@
 const completedButtons = document.getElementsByClassName('completed-btn');
 const assignedTaskCounter = document.getElementById('assigned-task-counter');
 const completedTaskCounter = document.getElementById('completed-task-counter');
+const historyItemsContainer = document.getElementById('history-items-container');
 
 
 for (button of completedButtons) {
     button.addEventListener('click', function (e) {
-
         alert('Task Completed!');
+
         e.target.setAttribute('disabled', 'true');
         e.target.style.backgroundColor = 'gray';
         e.target.style.cursor = 'no-drop';
         let assignedTasks = parseInt(assignedTaskCounter.innerHTML);
         let completedTasks = parseInt(completedTaskCounter.innerHTML);
 
+        
+        const selectedCard = e.target.parentElement.parentElement;
+        const cardTitle = selectedCard.querySelector('.task-title').innerHTML;
+        const newHistory = document.createElement('p');
+        newHistory.innerText='You have completed the task '+cardTitle;
+        newHistory.classList.add('history-item')
+        historyItemsContainer.appendChild(newHistory);
+
+
+       
 
         if (assignedTasks === 1) {
             alert('All Tasks Completed!');
